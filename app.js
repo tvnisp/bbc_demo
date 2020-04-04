1e25760ed58c1941f276faf29b991154c6aa688e
Panagiotis Gkionis <tvnisp@users.noreply.github.com>
2020-04-04 20:56:12 +0300

    organise code

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express   = require("express"),
-      app       = express(),
-      bodyParser= require("body-parser"),
-      port      = 5000;
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  port = 5000;
 
 //Set the app
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // RESTful Routes
 app.get("/", (req, res) => {
   res.render("index");
-})
+});
 
 app.get("/articles", (req, res) => {
   res.render("articles");
-})
+});
 
 app.post("/articles", (req, res) => {
-  res.render("sent", {review : req.body.review});
-})
+  res.render("sent", { review: req.body.review });
+});
 
 app.listen(process.env.PORT || port, () => {
   console.log("The app is running");
-})
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
 // IIFE function
-const articles = (function(){
-  let articles = [1, 2 ,3 ,4 ,5],
-      articlesUnordered = shuffle(articles),
-      count = 0;
-      // articlesObject = [];
+const articles = (function () {
+  let articles = [1, 2, 3, 4, 5],
+    articlesUnordered = shuffle(articles),
+    count = 0;
+  // articlesObject = [];
 
   // Preload all 5 articles and load them in an object array
   // function preloadArticles(){
@@ -23,7 +23,7 @@ const articles = (function(){
     nextArticle: document.querySelector("#next-article"),
     submitFormula: document.querySelector(".submit-formula"),
     lastArticle: document.querySelector("#last-article"),
-    loader: document.querySelector(".loader")
+    loader: document.querySelector(".loader"),
     // heading: document.querySelector(".heading"),
     // img: document.querySelector(".img"),
     // paragraph: document.querySelector(".paragraph"),
@@ -31,54 +31,60 @@ const articles = (function(){
   };
 
   // Return promise from json file - Article
-  function renderArticle(){
+  function renderArticle() {
     getArticle(articlesUnordered[count])
-    .then(data => {
-      DOM.loader.style = "display: none";
-      displayArticle(data);
-    })
-    .catch(error => {
-      displayError(error);
-    })
-  };
+      .then((data) => {
+        DOM.loader.style = "display: none";
+        displayArticle(data);
+      })
+      .catch((error) => {
+        displayError(error);
+      });
+  }
 
   // New image - Otherwhise won't load different images on the same page
-  function newImage(data){
-    let random = Math.floor(Math.random()*1000);
+  function newImage(data) {
+    let random = Math.floor(Math.random() * 1000);
     let image = new Image();
     image.src = `${data.model.url}=sig${random}`;
     image.alt = data.model.altText;
     DOM.article.appendChild(image);
-  };
+  }
 
   // Display error - If promise catches an error, will be displayed in the title
   function displayError(data) {
     DOM.article.innerHTML = `<h1>${data}, Please check your internet connection and try again. Press the Home button from the navigation bar.</h1>`;
-  };
+  }
 
   // Display the article
   function displayArticle(data) {
-    DOM.count.innerText = `Article ${count +1}/5`;
-    DOM.article.insertAdjacentHTML("beforeend", `<h1 class="title">${data.title}</h1>`);
-    data.body.forEach(el => {
+    DOM.count.innerText = `Article ${count + 1}/5`;
+    DOM.article.insertAdjacentHTML(
+      "beforeend",
+      `<h1 class="title">${data.title}</h1>`
+    );
+    data.body.forEach((el) => {
       if (el.type == "heading") {
-        DOM.article.insertAdjacentHTML("beforeend", `<h2>${el.model.text}</h2>`);
-      };
+        DOM.article.insertAdjacentHTML(
+          "beforeend",
+          `<h2>${el.model.text}</h2>`
+        );
+      }
       if (el.type == "image") {
         newImage(el);
-      };
+      }
       if (el.type == "paragraph") {
         DOM.article.insertAdjacentHTML("beforeend", `<p>${el.model.text}</p>`);
-      };
+      }
       if (el.type == "list") {
-        el.model.items.forEach(el => {
+        el.model.items.forEach((el) => {
           DOM.article.insertAdjacentHTML("beforeend", `<li>${el}</li>`);
         });
-      };
+      }
       DOM.nextArticle.style = "display: block";
       checkButton();
     });
-  };
+  }
 
   // Display ranking
   function displayRanking() {
@@ -86,25 +92,29 @@ const articles = (function(){
     DOM.nextArticle.style = "display: none";
     DOM.submitFormula.style = "display: flex";
     DOM.count.style = "display: none";
-  };
+  }
 
   //Clean last article
   function cleanLast() {
     DOM.article.innerHTML = "";
     DOM.nextArticle.style = "display: none";
     DOM.lastArticle.style = "display: none";
-  };
+  }
 
   //Load an article - New browsers
   async function getArticle(num) {
-    let response = await fetch(`https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-${num}.json`);
+    let response = await fetch(
+      `https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-${num}.json`
+    );
     let data = await response.json();
     return data;
-  };
+  }
 
   // Array randomizer
   function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
     while (currentIndex != 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
@@ -113,33 +123,31 @@ const articles = (function(){
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
-    };
-    return array;
-  };
-
-    // Display back button
-    function checkButton() {
-      if(count == 0 || count >= 5) {
-        DOM.lastArticle.style = "display: none";
-      } else {
-        DOM.lastArticle.style = "display: block";
-      }
     }
+    return array;
+  }
+
+  // Display back button
+  function checkButton() {
+    if (count == 0 || count >= 5) {
+      DOM.lastArticle.style = "display: none";
+    } else {
+      DOM.lastArticle.style = "display: block";
+    }
+  }
 
   // Control view model
   function controlView() {
-    // Display or not back button
-    checkButton();
     // Check if more articles
-    if (count < 5) {   
+    if (count < 5) {
       cleanLast();
       DOM.loader.style = "display: block";
       renderArticle();
     } else {
       cleanLast();
       displayRanking();
-    };
-  };
+    }
+  }
 
   // Event listeners
   DOM.nextArticle.addEventListener("click", () => {
@@ -147,17 +155,17 @@ const articles = (function(){
     controlView();
   });
 
-  DOM.lastArticle.addEventListener("click", () => {   
-      count--;
-      controlView();
+  DOM.lastArticle.addEventListener("click", () => {
+    count--;
+    controlView();
   });
 
   return {
     init: renderArticle(),
     // cache: cacheArticles()
-  }
+  };
 })();
 
-  // Load the first article onload
-  // articles.cache();
-  articles.init();
+// Load the first article onload
+// articles.cache();
+articles.init();
